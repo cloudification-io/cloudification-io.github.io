f782a5b6befa8e0a10176b596ced89f521f68915
Vadim Ponomarev <vadim.ponomarev@cloudification.io>
2024-03-03 19:00:22 +0100

    Add script for releasing

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for chart in memcached openvswitch ovn libvirt helm-toolkit; do
+     helm package ../openstack-helm-infra/$chart
+done
+
+for chart in cinder glance heat horizon keystone nova placement neutron; do
+     helm package ../openstack-helm/$chart
+done
+
+helm repo index . --url https://cloudification-io.github.io
+
+sed -i 's/file:.*helm-toolkit/https:\/\/cloudification-io.github.io\/helm-toolkit/g' index.yaml
